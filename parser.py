551f4cffd57119248efc5ce4f3c24829f49fff09
JHatredT <jhatredt@outlook.com>
2025-01-17 22:30:22 +0800

    Add support for expression grouping with parentheses

--- parser.py
+++ parser.py
@@ -3,51 +3,88 @@ from typing import List, Callable
 
 
 class Parser:
-    def __init__(self):
-        self._ptr = 0
-
-    def parse(self, expr: str):
-        self._ptr = 0
+    def parse(self, expr: str) -> float:
+        index = 0
         output = []
-        stack = []
+        op_stack = []
 
-        while self._ptr < len(expr):
-            if self.is_digit_or_dot(expr):
-                output.append(self.parse_num(expr))
-            elif expr[self._ptr] in func_tbl:
-                self.parse_func(expr, output, stack)
-
-        while stack:
-            if len(output) < 2:
+        while index < len(expr):
+            if self._is_digit_or_dot(expr, index):
+                index = self._parse_num(expr, index, output)
+            elif expr[index] in func_tbl:
+                index = self._parse_func(expr, index, output, op_stack)
+            elif expr[index] == '(':
+                op_stack.append('(')
+                index += 1
+            elif expr[index] == ')':
+                index = self._parse_paren(expr, index, output, op_stack)
+            else:
                 raise ValueError
 
-            output.append(stack.pop()(output.pop(-2), output.pop()))
+        while op_stack and op_stack[-1] in func_tbl:
+            self._apply_func(output, op_stack)
 
-        if not output or len(output) > 1:
+        if op_stack or not output or len(output) > 1:
             raise ValueError
 
         return output[0]
 
-    def parse_num(self, expr: str):
-        base = self._ptr
+    def _parse_num(self, expr: str, ptr: int, out: List[float]) -> int:
+        base = ptr
 
-        while self._ptr < len(expr) and self.is_digit_or_dot(expr):
-            self._ptr += 1
+        while ptr < len(expr) and self._is_digit_or_dot(expr, ptr):
+            ptr += 1
 
-        if expr[base:self._ptr].count('.') > 1:
+        if expr[base:ptr].count('.') > 1:
             raise ValueError
 
-        return float(expr[base:self._ptr])
+        out.append(float(expr[base:ptr]))
+        return ptr
 
-    def parse_func(self, expr: str, output: List[float], stack: List[str]):
-        while stack and func_tbl[expr[self._ptr]].prio <= stack[-1].prio:
-            if len(output) < 2:
-                raise ValueError
+    def _parse_func(
+        self,
+        expr: str,
+        ptr: int,
+        out: List[float],
+        stack: List[str]
+    ):
+        if stack and stack[-1] not in func_tbl:
+            stack.append(expr[ptr])
+            ptr += 1
+            return ptr
 
-            output.append(stack.pop()(output.pop(-2), output.pop()))
+        while stack and stack[-1] in func_tbl:
+            if not func_tbl[expr[ptr]].prio <= func_tbl[stack[-1]].prio:
+                break
+                
+            self._apply_func(out, stack)
 
-        stack.append(func_tbl[expr[self._ptr]])
-        self._ptr += 1
+        stack.append(expr[ptr])
+        ptr += 1
+        return ptr
 
-    def is_digit_or_dot(self, expr: str):
-        return expr[self._ptr].isdigit() or expr[self._ptr] == '.'
+    def _parse_paren(
+        self,
+        expr: str,
+        ptr: int,
+        out: List[float],
+        stack: List[str]
+    ):
+        while stack and stack[-1] != '(':
+            self._apply_func(out, stack)
+
+        if not stack:
+            raise ValueError
+
+        stack.pop()
+        ptr += 1
+        return ptr
+
+    def _apply_func(self, out: List[float], stack: List[str]):
+        if len(out) < 2:
+            raise ValueError
+
+        out.append(func_tbl[stack.pop()](out.pop(-2), out.pop()))
+
+    def _is_digit_or_dot(self, expr: str, ptr: int):
+        return expr[ptr].isdigit() or expr[ptr] == '.'
--- test.py
+++ test.py
@@ -9,14 +9,18 @@ class TestParseFunction(TestCase):
     def test_parse_valid(self) -> None:
         parser = Parser()
 
-        self.assertEqual(parser.parse("3+5−2×4÷2"), 4)
-        self.assertEqual(parser.parse("6×3÷2+7−1"), 15)
-        self.assertEqual(parser.parse("9−4+8×2÷5"), eval('9-4+8*2/5'))
-        self.assertEqual(parser.parse("2×7−5÷3+6"), eval('2*7-5/3+6'))
-        self.assertEqual(parser.parse("5−3+6×2÷7"), eval('5-3+6*2/7'))
-        self.assertEqual(parser.parse("4×5−2÷6+3"), eval('4*5-2/6+3'))
-        self.assertEqual(parser.parse("7+8÷4×3−2"), 11)
-        self.assertEqual(parser.parse("6×2−3÷1+5"), 14)
+        self.assertEqual(parser.parse('3+5−2×4÷2'), 4)
+        self.assertEqual(parser.parse('6×3÷2+7−1'), 15)
+        self.assertEqual(parser.parse('9−4+8×2÷5'), eval('9-4+8*2/5'))
+        self.assertEqual(parser.parse('2×7−5÷3+6'), eval('2*7-5/3+6'))
+        self.assertEqual(parser.parse('5−3+6×2÷7'), eval('5-3+6*2/7'))
+        self.assertEqual(parser.parse('4×5−2÷6+3'), eval('4*5-2/6+3'))
+        self.assertEqual(parser.parse('7+8÷4×3−2'), 11)
+        self.assertEqual(parser.parse('6×2−3÷1+5'), 14)
+        self.assertEqual(parser.parse('2+8÷(2×4)−2'), 1)
+        self.assertEqual(parser.parse('3+(5−1)÷2'), 5)
+        self.assertEqual(parser.parse('7+(2÷5)×(4−6)'), eval('7+(2/5)*(4-6)'))
+        self.assertEqual(parser.parse('((3+2)×(9−8)+1)+5÷2'), eval('((3+2)*(9-8)+1)+5/2'))
 
     def test_parse_invalid(self) -> None:
         parser = Parser()
@@ -28,6 +32,10 @@ class TestParseFunction(TestCase):
         self.assertRaises(ValueError, parser.parse, '8÷2×−3+4')
         self.assertRaises(ValueError, parser.parse, '5+×6−2÷')
         self.assertRaises(ValueError, parser.parse, '2×−5÷3+')
+        self.assertRaises(ValueError, parser.parse, '+7−4×8÷2')
+        self.assertRaises(ValueError, parser.parse, '(+2×7)-8÷2')
+        self.assertRaises(ValueError, parser.parse, ')9-3(−2×3÷1')
+        self.assertRaises(ValueError, parser.parse, '3−5×((7÷6)+1')
         self.assertRaises(ValueError, parser.parse, '+7−4×8÷2')
 
 
